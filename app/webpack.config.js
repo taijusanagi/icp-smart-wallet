782a32a92a339dc3e0bffe6ad1b84f00ed3a33b8
taijusanagi <taijusanagi@gmail.com>
2023-09-22 09:31:08 +0700

    integrate internet identity

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,13 +1,41 @@
-require("dotenv").config();
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
+const network = process.env.DFX_NETWORK || (process.env.NODE_ENV === "production" ? "ic" : "local");
+function initCanisterEnv() {
+  let localCanisters, prodCanisters;
+  try {
+    localCanisters = require(path.resolve(".dfx", "local", "canister_ids.json"));
+  } catch (error) {
+    console.log("No local canister_ids.json found. Continuing production");
+  }
+  try {
+    prodCanisters = require(path.resolve("canister_ids.json"));
+  } catch (error) {
+    console.log("No production canister_ids.json found. Continuing with local");
+  }
+
+  const canisterConfig = network === "local" ? localCanisters : prodCanisters;
+
+  return Object.entries(canisterConfig).reduce((prev, current) => {
+    const [canisterName, canisterDetails] = current;
+    prev[canisterName.toUpperCase() + "_CANISTER_ID"] = canisterDetails[network];
+    return prev;
+  }, {});
+}
+const canisterEnvVariables = initCanisterEnv();
+
 const isDevelopment = process.env.NODE_ENV !== "production";
 
-const frontendDirectory = "app_frontend";
+const internetIdentityUrl =
+  network === "local"
+    ? `http://localhost:4943/?canisterId=${canisterEnvVariables["INTERNET_IDENTITY_CANISTER_ID"]}`
+    : `https://identity.ic0.app`;
+
+const frontendDirectory = "greet_frontend";
 
 const frontend_entry = path.join("src", frontendDirectory, "src", "index.html");
 
@@ -55,13 +83,11 @@ module.exports = {
       template: path.join(__dirname, frontend_entry),
       cache: false,
     }),
-    new webpack.EnvironmentPlugin([
-      ...Object.keys(process.env).filter((key) => {
-        if (key.includes("CANISTER")) return true;
-        if (key.includes("DFX")) return true;
-        return false;
-      }),
-    ]),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: "development",
+      II_URL: internetIdentityUrl,
+      ...canisterEnvVariables,
+    }),
     new webpack.ProvidePlugin({
       Buffer: [require.resolve("buffer/"), "Buffer"],
       process: require.resolve("process/browser"),
